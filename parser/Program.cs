daaa6de2b1e46bdb296ceba254378a595dffba58
Kirill <135825784+reEcho0@users.noreply.github.com>
2024-06-13 22:07:29 +0300

    update parser
    данные получены с сайта gogov и приведены к читаемому виду

--- parser/Program.cs
+++ parser/Program.cs
@@ -1,5 +1,7 @@
 ﻿using HtmlAgilityPack;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Praktika
 {
@@ -8,29 +10,62 @@ namespace Praktika
         static void Main()
         {
             // Загрузите HTML-документ из файла или URL-адреса
-
             var htmlDocument = new HtmlWeb().Load("https://gogov.ru/natural-increase/rt");
 
             // Выберите узлы(nodes) с помощью XPath 
+            // Самые свежие данные на текущий год, не подходят для статистики, используются для общей информации и наполнения
             var actualDate = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='gu-data']//p//b").InnerHtml;
-            //var nodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='gu-data']//ul//li");
-            var birthrate = htmlDocument.DocumentNode.SelectNodes(
-                "/html/body/script[11]");
-            Console.WriteLine($"данные на {actualDate}\n");
-            // Извлеките и отобразите данные 
-            //foreach (var node in nodes)
-            //{
-            //    string str = node.InnerHtml.Remove(0, 3).Replace("</b>", string.Empty); ;                
-            //    Console.WriteLine($"{str}\n");
-            //}
-            foreach(var br in birthrate)
-            {
-                Console.WriteLine(br.InnerText);
-            }
+            var nodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='gu-data']//ul//li");
             
-
+            // Получение конкретных данных за 9 лет + последние данные
+            var data = htmlDocument.DocumentNode.SelectSingleNode(
+                "/html/body/script[11]");
+            
+            // Извлечение и отображение данных 
+            // Самые свежие данные            
+            Console.WriteLine($"данные на {actualDate}\n");
+            foreach (var node in nodes)
+            {
+                string str = Regex.Replace(node.InnerText,@"<b>(.*?)</b>",string.Empty);
+                Console.WriteLine($"{str}");
+            }
+            Console.WriteLine();
+            
+            // Лист "года" (в идеале конвертировать из строки в инт)
+            Match matchYears = Regex.Match(data.InnerHtml, @"\{ categories: \[(.*?)\]");
+            List<string> years = Regex.Match(matchYears.ToString(),@"\[(.*?)\]")
+                .ToString().Trim('[',']').Split(',').ToList();
+            years.RemoveRange(years.Count - 2, 2);
+            foreach (var year in years)
+            {
+                Console.Write($"{year.Trim('\'')}\t");
+            }
+            Console.WriteLine();
+            
+            // Словарь c данными для БД
+            Regex patternBirthrate = new Regex( @"\{ name: (.*?)\]");
+            MatchCollection birthrate = patternBirthrate.Matches(data.InnerHtml);           
+            foreach (Match match in birthrate)
+            {
+                List<int> numData = Regex.Match(match.Value, @"\[(.*?)\]").ToString().Trim('[', ']')
+                    .ToString().Split(',').Select(s=>int.Parse(s)).ToList();
+                numData.RemoveAt(numData.Count - 1);
+                Dictionary<string, List<int>> structured_data = new Dictionary<string, List<int>>()
+                {
+                    {Regex.Match(match.Value, @"\'(.*?)\'").ToString().Trim('\''), numData}
+                };
+                foreach(var structured in structured_data)
+                {
+                    Console.WriteLine(structured.Key);
+                    foreach(var num in structured.Value)
+                    {
+                        Console.Write($"{num}\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
-//node.InnerHtml
-//  /html/body/div[1]/div/div/div[3]/div/article/div/div[4]/div[2]/div[1]/div[1]/div/svg/g[7]/g[1]
+
+
